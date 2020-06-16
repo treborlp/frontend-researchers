973a424510930a077973c389def26b4204e87ead
Robert Alvarado <treborlp@gmail.com>
2020-06-16 13:11:57 -0500

    Create searcher profile and admin section

---
+++ src/app/admin/class/researcher.ts
@@ -0,0 +1,12 @@
+export class Researcher {
+    id: number;
+    name: String;
+    lastname: String;
+    maxDegree: String;
+    photo: String;
+    email: String;
+    cellphone: String;
+    profileCTIVitae: String;
+    profileGoogleScholar: String;
+    profileOrcid: String
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
+import { RouterModule, Routes} from '@angular/router'; // Necesario para rutas
 
 import { AppComponent } from './app.component';
+import { ResearcherComponent } from './admin/researcher/researcher.component';
+import { ProfilesComponent } from './public/profiles/profiles.component';
+
+//objeto de rutas
+const routes: Routes = [
+  {path:'', redirectTo:'public/researcher', pathMatch: 'full'},
+  {path:'public/researcher', component: ProfilesComponent },
+  {path:'admin/researcher', component: ResearcherComponent },
+
+  
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ResearcherComponent,
+    ProfilesComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes) //Agregamos las rutas con el objeto creado
   ],
   providers: [],
   bootstrap: [AppComponent]
